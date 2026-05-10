6f17a2def404cfa276eb14c7c799d5633b8762ff
ManiRednam <manirednam@gmail.com>
2026-05-10 12:11:51 +0530

    first commit

---
+++ database/connection.py
@@ -0,0 +1,11 @@
+import mysql.connector as SQLC
+
+# database configuration
+def databaseConfig():
+    db_config = SQLC.connect(
+        host='localhost',
+        user = 'root',
+        password='mani999',
+        database = 'ecommerce1'
+    )
+    return db_config
